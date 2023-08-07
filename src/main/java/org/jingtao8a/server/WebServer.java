ee21f7d1a7a37ae33ed4f775d4b2cc712263de76
yuxintao <1921056015@qq.com>
2023-08-07 16:45:24 +0800

    完成一个基于selector的单线程echo服务器

--- src/main/java/org/jingtao8a/server/WebServer.java
+++ src/main/java/org/jingtao8a/server/WebServer.java
@@ -6,10 +6,13 @@ import org.jingtao8a.timer.TimerManager;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class WebServer {
     private ServerSocketChannel serverSocketChannel;//监听套接字channel
@@ -23,20 +26,62 @@ public class WebServer {
         serverSocketChannel.bind(new InetSocketAddress(9999));
         serverSocketChannel.configureBlocking(false);
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-        timerManager.addTimer(new Timer(new Task(), 3000));
+    }
+
+    private void dealWithAccept(SelectionKey key) throws IOException {
+        SocketChannel clientChannel = ((ServerSocketChannel)key.channel()).accept();
+        clientChannel.configureBlocking(false);
+        clientChannel.register(selector, SelectionKey.OP_READ);
+    }
+
+    private void dealWithRead(SelectionKey key) throws IOException {
+        SocketChannel clientChannel = (SocketChannel) key.channel();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+        int count = clientChannel.read(buffer);
+        StringBuffer stringBuffer = new StringBuffer();
+        if (count > 0) {
+            buffer.flip();
+            stringBuffer.append(new String(buffer.array(), 0, count));
+        } else {
+            key.cancel();
+            clientChannel.close();
+            return;
+        }
+        String str = stringBuffer.toString();
+        Log.Info("%s", str);
+        key.attach(str);
+        key.interestOps(SelectionKey.OP_WRITE);
+    }
+
+    private void dealWithWrite(SelectionKey key) throws IOException {
+        SocketChannel clientChannel = (SocketChannel)key.channel();
+        String str = (String)key.attachment();
+        key.attach("");
+        clientChannel.write(ByteBuffer.wrap(str.getBytes()));
+        key.interestOps(SelectionKey.OP_READ);
     }
 
     public void eventLoop() throws IOException {
         while (true) {
             int res = selector.select(timeSlot);
             timerManager.doExpiredTask();
-        }
-    }
-    private class Task implements Runnable {
-        @Override
-        public void run() {
-            Log.Info("%d", System.currentTimeMillis());
-            timerManager.addTimer(new Timer(new Task(), 2000));
+            if (res > 0) {
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+                while (keyIterator.hasNext()) {
+                    SelectionKey key = keyIterator.next();
+                    keyIterator.remove();
+
+                    if (key.isAcceptable()) {
+                        dealWithAccept(key);
+                    } else if (key.isReadable()) {
+                        dealWithRead(key);
+                    } else if (key.isWritable()) {
+                        dealWithWrite(key);
+                    }
+                }
+            }
         }
     }
 }
