8c4ee312ebf3971cad42bf95ae9223a8f5dba391
yuxintao <1921056015@qq.com>
2023-08-10 20:59:41 +0800

    完成java_webserver,还有图片编码问题未解决

--- src/main/java/org/jingtao8a/http/HttpHandler.java
+++ src/main/java/org/jingtao8a/http/HttpHandler.java
@@ -302,7 +302,7 @@ public class HttpHandler {
                             uri = "/registerError.html";
                         } else {
                             Statement insertStmt = connection.createStatement();
-                            insertStmt.executeUpdate("insert into users values(" + name + "," + password + ")");
+                            insertStmt.executeUpdate("insert into users(name, password) values(\"" + name + "\",\"" + password + "\")");
                             uri = "/log.html";
                         }
                     } else if (uri.charAt(1) == '2') {// 登入
